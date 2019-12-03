5a7bd3005c4275463c78a5113555f017032aec53
Michael R Verhoeven <verh0064@ad.umn.edu>
2019-12-03 10:23:39 -0600

    Adds code to clean up field names

--- a_compiling_survey_data.R
+++ a_compiling_survey_data.R
@@ -732,7 +732,7 @@ unique(ps$dowid)
 ps <- tbl_df(ps)
 ps[] <- lapply(ps[], factor)
 
-# 2018 - AllisonGamble ----------------------------------------------------
+# 2018 - Allison Gamble ----------------------------------------------------
 
 
 #' ## Gamble Surveys:
@@ -786,7 +786,7 @@ for  (i in c(1:length(files))) {
 nrow(unique(cbind(ps$datemv, ps$datasourcemv, ps$lknamemv)))
 
 
-# 2018 - AndreaPrichard ----------------------------------------------------
+# 2018 - Andrea Prichard ----------------------------------------------------
 
 
 #' ## Andrea Prichard Surveys:
@@ -838,7 +838,7 @@ for  (i in c(1:length(files))) {
 
 nrow(unique(cbind(ps$datemv, ps$datasourcemv, ps$lknamemv)))
 
-# 2018 - AprilLondo ----------------------------------------------------
+# 2018 - April Londo ----------------------------------------------------
 
 
 #' ## DNR R3S Surveys:
@@ -891,7 +891,7 @@ for  (i in c(1:length(files))) {
 
 nrow(unique(cbind(ps$datemv, ps$datasourcemv, ps$lknamemv)))
 
-# 2018 - BrittaBelden ----------------------------------------------------
+# 2018 - Britta Belden ----------------------------------------------------
 
 
 #' ## CRWD Surveys:
@@ -2261,6 +2261,10 @@ setwd("G:/My Drive/Documents/UMN/Grad School/Larkin Lab/R_projects/surveycollati
 # Set working directory back to project location
 # ps <- fread(file = "data/output/clp_2018dow_2019dow_surveys.csv")
 
+
+# drop duplicated surveys -------------------------------------------------
+
+
 ps[ , .N , by = .(datemv, dowid)  ]
 
 #create new, unique survey ID for each survey
@@ -2274,11 +2278,199 @@ duplicated(surveys[ , .(datemv,dowid) ,])
 surveys[duplicated(surveys[ , .(datemv,dowid) ,]), dup:= TRUE , ]
 ids_to_drop <- surveys[ dup == T , SURVEY_ID,]
 
-ps <- ps[!(SURVEY_ID %in% ids_to_drop), .N , .(datemv, dowid, datasourcemv, SURVEY_ID) ]
+ps[!(SURVEY_ID %in% ids_to_drop), .N , .(datemv, dowid, datasourcemv, SURVEY_ID) ]
+ps <- ps[!(SURVEY_ID %in% ids_to_drop), , ]
 
-###########################
+
+# clean up field names ----------------------------------------------------
+
+ps <- tbl_df(ps)
+ps[] <- lapply(ps[], factor)
+
+#drop columns with no data:
+
+ps <- ps[,colSums(is.na(ps))<nrow(ps)]
+
+#' # Old Variable Names
+#' 
+#' Pull all of the variable names (field names) off of the dataset, also attaching the lake and datasource names to those to help me identify the survey that I need to look at to interpret names
+fielduse <- data.frame()
+print(Sys.time())
+  start <- Sys.time()
+  for (i in 1:ncol(ps)) { 
+  
+  # code for troubleshooting:
+  #####
+  # i =1
+  # colnames(ps)[i] # name for column i
+  # fieldname = colnames(ps[i]) # fieldnames assigned value of column i
+  # unique(ps[which(is.na(ps[,i])==FALSE),c("datasourcemv","lknamemv")]) # for column i, which rows have non NA values? of these, what are the unique lkname and datasource combinations?
+  # str(ps[,i,])
+  # length(unique(ps[which(is.na(ps[,i])==FALSE),c("datasourcemv","lknamemv")])) == 1
+  #####
+  # use rowbind to grow a dataframe that is fieldname=old variable name, and data source, and lake name. Only include unique combinations of each datasource and lkname.
+  fielduse <-  rbind(fielduse,data.frame("fieldname" = colnames(ps[i]),
+                                         unique(ps[which(is.na(ps[,i])==FALSE),c("datasourcemv","lknamemv")])))
+  
+  }
+
+  Sys.time() - start
+
+#summary(fielduse)
+str(fielduse)
+#length(unique(fielduse[,1]))
+
+#' Save the field (or variable) name data as a .csv file in the clp_surveys folder
+write.csv(fielduse, file = "data/output/fieldkeystart.csv", row.names = F)
+
+#' # New Variable Names
+#' 
+#' Next, I went through fieldkeystart.csv and added a column for the new name that I would assign to each 
+#' old field name. This woudl serve as a lookup key to assign a coordinated set of names to the data.
+#' I used Skawinski 2014 (Aq plants of the upper midwest), Chadde 2011 (A great lakes wetland flora),
+#' and Borman et al 1997 (Throght the looking glass) to assign latin names to the names in the 
+#' submitted datasets. Where unneeded fields were in survey data, I assigned them the name delete to
+#' flag them for deletion later on (example total n native species, richness, etc.)
+#' I saved this new file as fieldkeydone.csv. (this process took about 10 hours).
+#' 
+#' Heres a look at what I came up with
+# to load in finished field key:
+fk3 <- read.csv(file = "") 
+str(fk3)
+
+#' Not bad, we went from 607 variables to 161. Lets take a look at the new names:
+sort(unique(fk3[,'mrvname']))
+
+#' # Assign New Variable Names to Data
+#' 
+#' Now we want to take the "mrvnames" and pop them in as new columnnames in our statewide dataset. 
+#' Delete the extra stuff (mrv name, lk name, other info columns) from our lookup table (only need fieldname and mrvname columns)
+str(fk3)
+fk3[,c(2,3,5,6)] <- NULL
+str(fk3)
+
+#' There are also two dummy columns that I used in assembly that we can delete right away( titled: "NA" and "X" )
+summary(ps[,'NA.'])
+summary(ps[,'X..'])
+ps$NA. <- NULL
+ps$X.. <- NULL
+
+#' now check to make sure that we have a full key for each unique column title in fieldkey 3:
+length(colnames(ps))
+length(unique(fk3$fieldname))
+
+#' test the lookup from the fieldkey 3 table
+# save the old names
+pst <- colnames(ps)
+length(pst)
+# colnames(ps) <- pst # used to reassign old names to ps
+
+#' Do all columnames have a match? Are they all unique matches?
+# # testing
+# a <- c(1,2,3,4,5)
+# key <- data.frame(old = c(1:5), new = c(3,3,3,5,5))
+# match(a,key$new)
+sum(is.na(match(pst, fk3[,1]))) # 0 NA matches
+length(unique(match(pst, fk3[,1]))) # 607 unique matches
+
+#' make a new columnname vector that has my new variable names in the order of the way they match the old dataset
+# make a vector that matches ps column names to my old names and assigns each object the new name (order of old dataset is maintained)
+pst1 <- fk3$mrvname[match(pst, fk3[,1])] 
+paste(pst1[345],names(ps[345]), sep= "---") # test one match
+
+#' # Collating Multiple Variables Into A Single Column
+#' 
+#' All columns contain data--yikes. How do we unite those duplicates into single columns (for example, hw do we combine all of our point.id columns?)
+#' 1. take all columns named "x" and combine them, separating their values with commas
+#' 2. remove all NAs
+#' 3. Review the data in each column, removing erroneous rows... This will be a big step.
+#' Let's start with 1&2
 
 
+#' Assign numbers to the new names as well (e.g., point.id_1, point.id_2, ..., point.id_i)
+# link up new and old names
+names <- data.frame(pst1, pst)
+length(names[,1])
+# alphabetize
+head(order(names[,1]))
+head(names[order(names[,1]),1:2])
+# alphabetize and tack on numbers
+names <- names[order(names[,1]),1:2] # make "names" into the ordered version
+names$pst3 <- paste(names[,1],1:length(names[,1]),sep = "_") # add numbers to the ordered version
+# keep the old names and the numbered new names:
+fk4 <- names[,2:3]
+
+#' now need to reorder these names based on the ps dataset
+# check matches
+sum(is.na(match(pst, fk4[,1]))) # how many NA matches?
+length(unique(match(pst, fk4[,1]))) # how many unique matches?
+# reorder and reassign names of ps dataset to be numbered new names
+pst2 <- fk4[match(pst, fk4[,1]),2] #change order back to the ps dataset order
+colnames(ps) <- pst2
+#' Define the new matches (pst are original names)!
+sort(paste(colnames(ps),pst, sep = "---"))
+
+#' Combine all rows with point.id_i (to develop the code for NA rm and collation of columns)  
+#####
+#' to pull the names w/o any numbers:
+head(word(string = names(ps), start = -2, sep = "_"))
+tail(word(string = names(ps), start = -2, sep = "_"))
+
+#' combine all columns with the same name
+head(names(ps)=="point.id") #which ps are named point.id
+head(word(string = names(ps), start = -2, sep = "_")=="point.id") # and after cutting off the number label?
+head(ps[,word(string = names(ps), start = -2, sep = "_")=="point.id"]) # call out the columns with point.id in them
+names(ps[,word(string = names(ps), start = -2, sep = "_")=="point.id"]) # the names of those columns
+
+#' use unite to paste together all point id column data
+ps.test <- unite_(ps, #dataframe
+                  col = "point.id", #new title
+                  names(ps[,word(string = names(ps), start = -2, sep = "_")=="point.id"]), # old titles
+                  sep = ",", # separate data with commas
+                  remove = TRUE #delete the old field headings
+)
+
+ps.test$point.id <- as.character(ps.test$point.id)
+# mutate to remove NAs
+head(sapply(ps.test,class))
+ps.test$point.id <- as.factor(ps.test$point.id)
+head(ps.test$point.id)
+head(summary(ps.test$point.id))
+#####
+
+#' At this point we have a point id variable that contains many NAs (to be deleted) and multiple erroneous (also need to delete these)
+#' We also need to verify that there are no "#,#" or "#,etc" left in here.
+#' Finally, we need to do this unite and subsequent NA deletion for all of the other variables in our dataset. 
+#' 
+#' A loop to unite all columns with the same name, then delete all of the NA's in em'(takes ~ 3 minutes to run)
+for (j in unique(pst1)) {
+  # testdat <- ps[1:100,] # used for testing             
+  # j = as.character(unique(pst1)[1]) # used for testing 
+  # unite to paste together all point id column data
+  ps <- unite_(ps, #dataframe
+               col = paste(j,"a",sep = "_") , # new title for created column--must keep "_a" ending to ensure that word() fn doesnt break in the next line
+               names(ps)[word(string = names(ps), start = -2, sep = "_") == j ], # old column titles to be united
+               sep = ",", # separate data from multiple columns with commas
+               remove = TRUE # delete the old field headings
+  )
+  # trim out NAs and commas
+  ps[,paste(j,"a",sep = "_")] = as.factor(TrimMult(na.remove(ps[,paste(j,"a",sep = "_")]), char = ","))
+  # progress:
+}
+
+#' # Check Result, Save Progress
+#' examine the new variables
+head(sort(names(ps)))
+str(ps)
+summary(ps$point.id_a)
+
+#' strip "_a" from the new field names (used in the collating process)
+# to pull the names w/o any "_a":
+# word(string = names(ps), start = -2, sep = "_")
+colnames(ps) <- word(string = names(ps), start = -2, sep = "_")
+sort(names(ps))
+
+#' save progress as a .csv file in the clp_surveys folder
 
 
 
